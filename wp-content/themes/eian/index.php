389831f5484beedf7b8e31bee3b8a4e72a94994e
PhoePA <pyaephyoaung.wcs@gmail.com>
2024-05-08 10:52:08 +0630

    first commit

---
+++ wp-content/themes/eian/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * main template fiel
+ * 
+ * 
+ * @package 
+ * 
+ *
+ * */
+ ?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eian's Wordpress Theme</title>
+</head>
+<body>
+    Hello Wordpress
+</body>
+</html>
